5aa0a771d4e30b6cb17ca6435b989f1312dc63fb
firvain <etsipis@terracognita.eu>
2022-09-22 09:14:20 +0300

    update scheduler add logging

--- Modules/Scheduler.py
+++ Modules/Scheduler.py
@@ -1,11 +1,16 @@
+import logging
 import time
-import os
-
 from apscheduler.triggers.cron import CronTrigger
-from colorama import Fore
-from colorama import Style
+
 from colorama import init
 from apscheduler.schedulers.background import BackgroundScheduler
+
+logname = 'scheduler.log'
+logging.basicConfig(filename=logname,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S')
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 init(autoreset=True)
 
@@ -17,18 +22,18 @@ def my_schedule(job1, job2, job3):
         year="*", month="*", day="*", hour="0", minute="5", second="0"
     )
     trigger_job2 = CronTrigger(
-        year="*", month="*", day="*", hour="0", minute="5", second="0"
+        year="*", month="*", day="*", hour="0", minute="10", second="0"
     )
     trigger_job3_a = CronTrigger(
-        year="*", month="*", day="*", hour="23", minute="55", second="0"
+        year="*", month="*", day="*", hour="23", minute="50", second="0"
     )
     trigger_job3_b = CronTrigger(
-        year="*", month="*", day="*", hour="11", minute="55", second="0"
+        year="*", month="*", day="*", hour="11", minute="50", second="0"
     )
     scheduler.add_job(job1, trigger=trigger_job1, name="daily pull sensor data")
     scheduler.add_job(job2, trigger=trigger_job2, name="daily pull openweather data")
-    scheduler.add_job(job2, trigger=trigger_job3_a, name="night pull accuweather data")
-    scheduler.add_job(job2, trigger=trigger_job3_b, name="day pull accuweather data")
+    scheduler.add_job(job3, trigger=trigger_job3_a, name="night pull accuweather data")
+    scheduler.add_job(job3, trigger=trigger_job3_b, name="day pull accuweather data")
     scheduler.start()
 
     try:
